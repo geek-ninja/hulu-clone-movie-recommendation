ce305306c2bcdffb9ea3327d59f1aa90b78b185f
Sanket Puhan <54413046+geek-ninja@users.noreply.github.com>
2021-07-21 18:50:39 +0530

    Add files via upload

---
+++ hulu/build/precache-manifest.cbbac53371cf800bc71173fa98ab190e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535f28286898a7dae402a9f973f7687f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41218d271e3a6c38da03",
+    "url": "/static/css/main.725d8205.chunk.css"
+  },
+  {
+    "revision": "9594e680639e3e8dfa0b",
+    "url": "/static/js/2.7357d4bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7357d4bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41218d271e3a6c38da03",
+    "url": "/static/js/main.ef46e9be.chunk.js"
+  },
+  {
+    "revision": "10e31650928e8e743aea",
+    "url": "/static/js/runtime-main.21462310.js"
+  }
+]);
